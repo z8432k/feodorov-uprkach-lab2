d8229d31a339af0734d3c1b4e4a2b1e2560bc05d
Alexander Feodorov <webmaster@unixcomp.org>
2022-01-31 18:27:35 +0300

    Front selects

--- services/var1/httpd/public-html/src/client/Data/Form.jsx
+++ services/var1/httpd/public-html/src/client/Data/Form.jsx
@@ -1,3 +1,5 @@
+import { useEffect, useState } from "react";
+
 function Select({label, options} =  { label: "", options: [] }) {
   return (<label>
     {label}
@@ -17,12 +19,24 @@ function Text({ label } = { label: "" }) {
 }
 
 export default function Form() {
+  const [init, setInit] = useState({
+    source: [],
+    target: [],
+    klass: []
+  });
+
+  useEffect(() => {
+    fetch("/cgi-bin/init.cgi")
+      .then(resp => resp.json())
+      .then(setInit);
+  }, []);
+
   return (<div>
     <p>Введите поисковый запрос.</p>
     <form>
-      <Select label="Откуда" options={[]} />&nbsp;
-      <Select label="Куда" options={[]} />&nbsp;
-      <Select label="Класс" options={[]} />&nbsp;
+      <Select label="Откуда" options={init.source.map(o => ({ label: o, value: o }))} />&nbsp;
+      <Select label="Куда" options={init.target.map(o => ({ label: o, value: o }))} />&nbsp;
+      <Select label="Класс" options={init.klass.map(o => ({ label: o, value: o }))} />&nbsp;
       <Text label="Дата отправл." />&nbsp;
       <button>Поиск</button>
     </form>
